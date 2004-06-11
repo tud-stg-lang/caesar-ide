83980dfe46b02424a178dd405125527e8b2ff830
cdt <cdt>
2004-06-11 09:03:53 +0000

    *** empty log message ***

--- org.caesarj.ui/src/org/caesarj/ui/marker/AdviceMarkerResolutionGenerator.java
+++ org.caesarj.ui/src/org/caesarj/ui/marker/AdviceMarkerResolutionGenerator.java
@@ -2,11 +2,20 @@ package org.caesarj.ui.marker;
 
 import org.apache.log4j.Logger;
 import org.aspectj.asm.LinkNode;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IFileEditorInput;
 import org.eclipse.ui.IMarkerResolution;
 import org.eclipse.ui.IMarkerResolutionGenerator;
 import org.eclipse.ui.IMarkerResolutionGenerator2;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.FileEditorInput;
 /**
  * @author Shadow
  *
@@ -35,5 +44,35 @@ public class AdviceMarkerResolutionGenerator implements IMarkerResolutionGenerat
 	 */
 	public boolean hasResolutions(IMarker marker) {
 		return true;
+	}
+	
+	public class AdviceMarkerResolution implements IMarkerResolution {
+		private LinkNode link;
+	
+		public AdviceMarkerResolution(LinkNode link) {
+			super();
+			this.link = link;
+		}
+
+		public String getLabel() {
+			return "Goto Advice: " + link.getName();
+		}
+
+		public void run(IMarker marker) {
+			IWorkbenchWindow w = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+			if (w == null)
+				return;
+			IWorkbenchPage page = w.getActivePage();
+			if (page == null)
+				return;
+			IFileEditorInput input = new FileEditorInput((IFile) marker.getResource());
+			IEditorPart editorPart = page.findEditor(input);
+			try {
+				editorPart = page.openEditor((IFile) marker.getResource());
+			} catch (PartInitException e) {
+				MessageDialog.openError(w.getShell(), "ERROR","Unable to open Editor!"); //$NON-NLS-1$
+			}
+		}
+
 	}
 }
